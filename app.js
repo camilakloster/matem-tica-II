41b3e297346d51603b49a729c6c02ecc5f78f471
camilakloster <159257977+camilakloster@users.noreply.github.com>
2024-09-13 10:30:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container 
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+}
+let respostaEstaVisivel = false
+function viraCartao(){
+    respostaestaVisivel = !respostaEstaVisivel
+}
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
